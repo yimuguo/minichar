dc6220fd06bb7b8ccacc575d6953a2fb733df2fc
yimuguo <guoyimu@gmail.com>
2015-11-11 13:27:24 -0800

    trial

--- aardvark_fw.py
+++ aardvark_fw.py
@@ -1,17 +1,17 @@
 import ctypes
-api = ctypes.cdll.LoadLibrary('.\\aardvark.dll')
-# try:
-#     import aardvark as api
-# except ImportError as ex1:
-#     import imp, platform
-#     ext = platform.system() in ('Windows', 'Microsoft') and '.dll' or '.so'
-#     try:
-#         api = imp.load_dynamic('aardvark', 'aardvark' + ext)
-#     except ImportError as ex2:
-#         import_err_msg  = 'Error importing aardvark%s\n' % ext
-#         import_err_msg += '  Architecture of aardvark%s may be wrong\n' % ext
-#         import_err_msg += '%s\n%s' % (ex1, ex2)
-#         raise ImportError(import_err_msg)
+# api = ctypes.cdll.LoadLibrary('.\\aardvark.dll')
+try:
+    import aardvark as api
+except ImportError, ex1:
+    import imp, platform
+    ext = platform.system() in ('Windows', 'Microsoft') and '.dll' or '.so'
+    try:
+        api = imp.load_dynamic('aardvark', 'aardvark' + ext)
+    except ImportError, ex2:
+        import_err_msg  = 'Error importing aardvark%s\n' % ext
+        import_err_msg += '  Architecture of aardvark%s may be wrong\n' % ext
+        import_err_msg += '%s\n%s' % (ex1, ex2)
+        raise ImportError(import_err_msg)
 
 # version = api.py_version
 # print(api.py_version)
